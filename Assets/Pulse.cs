8e9ba31d9a76493d45c6b477a4c93038eedff22e
shshwdr <cs.vivian04@gmail.com>
2023-03-29 01:39:31 -0700

    bug fixes

--- Assets/Pulse.cs
+++ Assets/Pulse.cs
@@ -8,6 +8,8 @@ public class Pulse : MonoBehaviour
     public float time = 1;
     public float moveDistance = 0.3f;
     public float offset = 0;
+
+    public bool isVerticle = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,16 @@ Invoke("move",offset);
         rend.color = new Color(1, 1, 1, 0f);
         DOTween.To(() => rend.color, x => rend.color = x, new Color(1, 1, 1, 1f), time / 2).SetLoops(-1, LoopType.Yoyo)
             .SetEase(Ease.Linear);
-        transform.DOLocalMoveX(moveDistance, time).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        if (isVerticle)
+        {
+            
+            transform.DOLocalMoveY(moveDistance, time).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        }
+        else
+        {
+            
+            transform.DOLocalMoveX(moveDistance, time).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        }
     }
 
     // Update is called once per frame
