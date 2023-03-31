9b25ea42e252c978ce72ef61d3c0fa2b22dee04c
shshwdr <cs.vivian04@gmail.com>
2023-03-30 18:51:18 -0700

    minor fix

--- Assets/TurnSlider.cs
+++ Assets/TurnSlider.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,13 +10,21 @@ public class TurnSlider : MonoBehaviour
     public Transform slider;
     public Transform textTrans;
     public Text text;
+
+    private void Start()
+    {
+        
+        textTrans.gameObject.SetActive(false);
+    }
+
     // Start is called before the first frame update
     public IEnumerator ShowSlider(string t)
     {
         
+        textTrans.gameObject.SetActive(true);
         text.text = t;
         var pos = textTrans.position;
-        pos .x=1440;
+        pos .x=2040;
         textTrans.position = pos;
         slider.DOScaleY(1, GridManager.animTime);
         textTrans.DOLocalMoveX(0, GridManager.animTime);
@@ -23,8 +32,8 @@ public class TurnSlider : MonoBehaviour
         yield return new WaitForSeconds(GridManager.animTime*2);
 
         slider.DOScaleY(0, GridManager.animTime);
-        textTrans.DOLocalMoveX(-1440, GridManager.animTime);
+        textTrans.DOLocalMoveX(-2040, GridManager.animTime);
         yield return new WaitForSeconds(GridManager.animTime);
-
+        textTrans.gameObject.SetActive(false);
     }
 }
